9a0ec2d4ed58411f7e76fe597b1f83dfb034360c
biplob14 <biploburrahaman@gmail.com>
2020-06-25 22:31:23 +0600

    frontend template

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>user count</title>
+</head>
+<body>
+    <div class="wrapper">
+        <h1>Visitor Count</h1>
+        <h3>433</h3>
+    </div>
+</body>
+</html>
